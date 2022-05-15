03ad1e39a76e77d59613208645126e64a7180d2e
Vladislav Igorevich <cool-smit3@yandex.ru>
2022-05-15 22:54:40 +0300

    Киррилицу читает и переводит ноесть нюансы

--- lessons01fullquests/QuestionFriday.php
+++ lessons01fullquests/QuestionFriday.php
@@ -40,11 +40,20 @@ require_once __DIR__ . '/function.php';
 //============================================
 //$textEn = 'Hello World!';
 $textEn = 'Everybody dance now!';
-$textRu = 'Делу время потехе час';
+//$textRu = 'Делу время потехе час';
+$textRu = 'Саша любит арбузы';
 $countEn = 0;
 $countRu = 0;
 
 
+$enWords = ['b' => 'B', 'c' => 'C', 'd' => 'D',
+    'f' => 'F', 'g' => 'G', 'h' => 'H', 'j' => 'J', 'k' => 'K',
+    'l' => 'L', 'm' => 'M', 'n' => 'N', 'p' => 'P', 'q' => 'Q',
+    'r' => 'R', 's' => 'S', 't' => 'T', 'v' => 'V', 'w' => 'W',
+    'Y' => 'Y', 'z' => 'Z',];
+
+$enWordsVow = ['a' => 'A', 'e' => 'E', 'i' => 'I',
+    'o' => 'O', 'u' => 'U'];
 
 
 $rusWordsVow = ['а' => 'А', 'и' => 'И', 'е' => 'Е',
@@ -59,10 +68,78 @@ $rusWords = ['б' => 'Б', 'в' => 'В', 'г' => 'Г',
     'ф' => 'Ф', 'х' => 'Х', 'ц' => 'Ц', 'ч' => 'Ч',
     'ш' => 'Ш', 'щ' => 'Щ',];
 
+
 var_dump($textRu);
 echo '<br>   ----------------TITLE--------------------------';
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//$test22 = 'гавно';
+//$countest = 0;
+//while (isset($test22[$countest])) { //подсчет массива (без пробелов он всегда четный)
+//    $countest++;
+//}
+//
+//
+//$test228 = [];
+//for ($i = 0; $i < $countest; $i++) {
+//    $test228[] = $test22[$i];
+//}
+//
+//                //ТЕСТИРОВАТЬ
+//for ($i = 0; $i < (count($test228) - 1); $i += 2) {
+//    foreach ($rusWordsVow as $small => $big) {
+//        if ($test228[$i] . $test228[$i + 1] == $small
+//            || $test228[$i] . $test228[$i + 1] == $big) {
+//            echo $test228[$i] . $test228[$i + 1].
+//                ' ну типо ранво '.$big . '|| '. $small ;
+//            echo '<br>';
+//        }
+//    }
+//}
+//
+//die;
+
+
+//foreach ($testing as $test) {
+//    $flag = false;
+//    echo '<br> TEST ==== '. $test;
+//    if ($test == ' ') {
+//        $res = $res . $test;
+//        $flag = false;
+//        //echo $test;
+//        echo ' $i = ' . $i . '$test == (space)';
+//    }
+//    foreach ($rusWords as $small => $big) {
+//        if ($test == $small || $test == $big) {
+//            $flag = true;                          //    согласная записана
+//            $trash = $test;
+//            $res = $res . $test;
+//            echo ' $i = ' . $i . '$test == $small || $big';
+//            echo '<br>  ----------------standart Words------------------<br>';
+//        }
+//    }
+//    if ($flag) {
+//        foreach ($rusWordsVow as $smallVow => $bigVow) {
+//            if ($test == $smallVow || $test == $bigVow) {  // если встретилась гласная
+//                $res = $res . $trash;
+//                echo ' $i = ' . $i . '$test == $smallVow || $bigVow';
+//            }
+//        }
+//    }
+//    $i++;
+//}
+//
+//var_dump($res);
+//echo $res;
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+$res = '';
+$consWord = '';
+$trash = '';
+$flag = false;
+$i = 0;
+
 
 while (isset($textRu[$countRu])) { //подсчет массива (без пробелов он всегда четный)
     $countRu++;
@@ -80,64 +157,59 @@ for ($i = 0; $i < $countRu; $i++) {
     }
 }
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-var_dump($spaces);
-echo '<br> --space-- <br>';
-
 for ($i = 0; $i < $countRu; $i++) {
     $testing[] = $textRu[$i];
 }
-
-var_dump($spaces);
-$consWord = '';
+//==================================================================================
+echo '<br>';
+$testRes = '';
 for ($i = 0; $i < $countRu - 1; $i += 2) {
-    $flag = false;
     for ($j = 0; $j < count($spaces); $j++) {
-        if ($i = $spaces[$j]) {
+        if ($i == $spaces[$j]) {
             $result[] = ' ';
+            $testRes .= ' ';
             $i += 1;
+            //continue;
             $consWord = ''; // Индекс попал на пробел
+            echo '<br>' . ($i - 1) . '=' . $spaces[$j] . '<br>';
         }
     }
+    echo $i . ' = ITERATION >>>>>>>>>>';
     foreach ($rusWords as $small => $big) {
-        if ($testing[$i] . $testing[$i + 1] === $small || $big) {
+        if ($testing[$i] . $testing[$i + 1] === $small ||
+            $testing[$i] . $testing[$i + 1] === $big) {
             $flag = true;                             // открытие доступа к гласной
             $consWord = $testing[$i] . $testing[$i + 1];  // Согласная записана
+            echo '<br> -- Согласная записана <br>';
+            $testRes .= $consWord;
         }
     }
 
     if ($flag) {  //  если согласная была записана
         foreach ($rusWordsVow as $smallVow => $bigVow) {                //
-            if ($testing[$i] . $testing[$i + 1] === $smallVow || $bigVow) {
+            if ($testing[$i] . $testing[$i + 1] === $smallVow ||
+                $testing[$i] . $testing[$i + 1] === $bigVow) {
                 $testing[$i] . $testing[$i + 1] = $consWord;
-
+                echo '<br> -- Гласная записана <br>';
+                $testRes .= $consWord;
+                $flag = false;
             }
         }
     }
+    else{
+        $testRes .= $testing[$i] . $testing[$i + 1];
+        echo '<br> -- текущая записана <br>';
+    }
 }
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//==================================================================================
 
-var_dump($testing);
+var_dump($testRes);
 
 echo '<br>';
 
 
 die;
 echo '<br>';
-$test = replaceRusVowels($textRu);
-
-echo '<br>';
-var_dump($test);
-
-////var_dump(replaceRusVowels($textRu));
-//$test = replaceRusVowels($textRu);
-//
-//foreach ($test as $word){
-//    echo $word;
-//}
-
-die;
 
 
 ?>
