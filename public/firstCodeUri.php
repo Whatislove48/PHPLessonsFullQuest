12136cb84336f3d12e86b333c0c596e17e575de3
Vlados <vlad-shinkarenko1999@mail.ru>
2022-06-21 18:02:29 +0300

    LVL02 - push

--- public/firstCodeUri.php
+++ public/firstCodeUri.php
@@ -1,36 +1,9 @@
 <?php
 
+/*
 require_once __DIR__ . '/../PHPlevel2/App/autoload.php';
 spl_autoload_register('autoload');
 
-
-$sum = function (int $x, int $y): int {
-    return $x + $y;
-};
-
-$dif = function (int $x, int $y): int {
-    return $x - $y;
-};
-
-function calc($x, $y, $op)
-{
-    return $op($x,$y);
-}
-
-echo calc(3, 7, $dif);
-echo '<hr>';
-
-$arr = [2,3,7,9,13];
-
-$res = array_map(function ($x) { return $x*$x;},$arr);
-var_dump($res);
-
-
-
-
-
-
-/*
 //$uri = $_SERVER['REQUEST_URI'];
 $uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
 
