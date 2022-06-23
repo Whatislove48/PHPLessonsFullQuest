ae90a69c61c76ced40991b183a94d0a5f4bb5872
Vlados <vlad-shinkarenko1999@mail.ru>
2022-06-23 18:07:41 +0300

    LVL02 - 2.8 begin

--- public/test.php
+++ public/test.php
@@ -3,17 +3,34 @@
 
 require_once __DIR__ . '/../PHPlevel2/App/autoload.php';
 
+echo 'Hello';
+echo '<br>';
 
-$functions =
-[
-    'title' => function(\App\Models\ArticleRep $article) {
-        return $article->getTitle();
-    },
-    'trimmedText' => function(\App\Models\ArticleRep $article) {
-        return mb_strimwidth($article->getText(), 0, 100);
-    }
-];
 
-$all = new \App\Repositories\ArticleRepository();
+$yii = dirname(__FILE__) .'/../vendor/yiisoft/yii/framework/yii.php' ;
+$config = dirname(__FILE__) .'/../vendor/yiisoft/yii/framework/yii.php' ;
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once $yii;
+
+
+Yii::createWebApplication($config)->run();
+
+
+
+// /home/roman/PhpstormProjects/localhost/PHPLessonsFullQuest/testdrive/../vendor/yiisoft/yii/framework/yii.php"
+// /home/roman/PhpstormProjects/localhost/PHPLessonsFullQuest/public/../vendor/yiisoft/yii/framework/yii.php
+
+
+
+
+
+
+
+
 
 
